9fcad763084f269b7c6b92439225a49ebfa69b8f
mjakobs <25904555+mjakobs@users.noreply.github.com>
2022-11-24 11:38:32 +0000

    updated unit test

--- tests/testthat/test_time_amplification.R
+++ tests/testthat/test_time_amplification.R
@@ -5,15 +5,15 @@ library(covr)
 #### test input ####
 test_that("AmplificationTimeR runs and produces a data.frame when all input is correct", {
   expect_equal(class(time_amplification(cn_data = test_data_cn,
-                                  multiplicity_data = test_data_mult,
-                                  mutation_data = test_data_muts,
-                                  muts_type = "SBS1 and SBS5",
-                                  sample_id = test_data_id,
-                                  amplification_chrom = test_data_chrom,
-                                  amplification_start = test_data_start,
-                                  amplification_stop = test_data_stop,
-                                  is_WGD = test_data_status,
-                                  genome = "hg19")),"data.frame")
+                                        multiplicity_data = test_data_mult,
+                                        mutation_data = test_data_muts,
+                                        muts_type = "SBS1 and SBS5",
+                                        sample_id = test_data_id,
+                                        amplification_chrom = test_data_chrom,
+                                        amplification_start = test_data_start,
+                                        amplification_stop = test_data_stop,
+                                        is_WGD = test_data_status,
+                                        genome = "hg19")),"data.frame")
 })
 
 # test copy number input
@@ -287,26 +287,26 @@ test_that("AmplificationTimeR runs and produces a data.frame when nMaj2_A and nM
 #### Check output ####
 # test output
 test_that("AmplificationTimeR runs and produces a data.frame with correct columns when all input is correct", {
-                                           expect_equal(colnames(time_amplification(cn_data = test_data_cn,
-                                                                                    multiplicity_data = test_data_mult,
-                                                                                    mutation_data = test_data_muts,
-                                                                                    muts_type = "SBS1 and SBS5",
-                                                                                    sample_id = test_data_id,
-                                                                                    amplification_chrom = test_data_chrom,
-                                                                                    amplification_start = test_data_start,
-                                                                                    amplification_stop = test_data_stop,
-                                                                                    is_WGD = test_data_status,
-                                                                                    genome = "hg19")),c("sample","region","highest_copy_number","event_order","num_mutations_used","clonality_status",
-                                                                                                                  "t_1","t_1_median_bootstrap","t_1_lower_ci","t_1_upper_ci",
-                                                                                                                  "t_2","t_2_median_bootstrap","t_2_lower_ci","t_2_upper_ci",
-                                                                                                                  "t_3","t_3_median_bootstrap","t_3_lower_ci","t_3_upper_ci",
-                                                                                                                  "t_4","t_4_median_bootstrap","t_4_lower_ci","t_4_upper_ci",
-                                                                                                                  "t_5","t_5_median_bootstrap","t_5_lower_ci","t_5_upper_ci",
-                                                                                                                  "t_6","t_6_median_bootstrap","t_6_lower_ci","t_6_upper_ci",
-                                                                                                                  "t_7","t_7_median_bootstrap","t_7_lower_ci","t_7_upper_ci",
-                                                                                                                  "t_8","t_8_median_bootstrap","t_8_lower_ci","t_8_upper_ci",
-                                                                                                                  "t_9","t_9_median_bootstrap","t_9_lower_ci","t_9_upper_ci",
-                                                                                                                  "t_10","t_10_median_bootstrap","t_10_lower_ci","t_10_upper_ci"))
+  expect_equal(colnames(time_amplification(cn_data = test_data_cn,
+                                           multiplicity_data = test_data_mult,
+                                           mutation_data = test_data_muts,
+                                           muts_type = "SBS1 and SBS5",
+                                           sample_id = test_data_id,
+                                           amplification_chrom = test_data_chrom,
+                                           amplification_start = test_data_start,
+                                           amplification_stop = test_data_stop,
+                                           is_WGD = test_data_status,
+                                           genome = "hg19")),c("sample","region","highest_copy_number","event_order","num_mutations_used","clonality_status",
+                                                               "t_1","t_1_median_bootstrap","t_1_lower_ci","t_1_upper_ci",
+                                                               "t_2","t_2_median_bootstrap","t_2_lower_ci","t_2_upper_ci",
+                                                               "t_3","t_3_median_bootstrap","t_3_lower_ci","t_3_upper_ci",
+                                                               "t_4","t_4_median_bootstrap","t_4_lower_ci","t_4_upper_ci",
+                                                               "t_5","t_5_median_bootstrap","t_5_lower_ci","t_5_upper_ci",
+                                                               "t_6","t_6_median_bootstrap","t_6_lower_ci","t_6_upper_ci",
+                                                               "t_7","t_7_median_bootstrap","t_7_lower_ci","t_7_upper_ci",
+                                                               "t_8","t_8_median_bootstrap","t_8_lower_ci","t_8_upper_ci",
+                                                               "t_9","t_9_median_bootstrap","t_9_lower_ci","t_9_upper_ci",
+                                                               "t_10","t_10_median_bootstrap","t_10_lower_ci","t_10_upper_ci"))
 })
 
 # test that expected output is correct when input is correct
@@ -2200,4 +2200,22 @@ test_that("AmplificationTimeR runs and produces 10+2 highest output and order", 
                                   amplification_stop = test_data_stop,
                                   is_WGD = TRUE,
                                   genome = "hg19")[,"event_order"],"GGGGW")
+})
+
+
+#### Test C>G at CpG code ####
+library(BSgenome.Hsapiens.UCSC.hg19)
+test_that("AmplificationTimeR runs and produces a data.frame when all input is correct", {
+  expect_equal(class(
+    time_amplification(cn_data = test_data_cn_ctcpg,
+                       multiplicity_data = test_data_mult_ctcpg,
+                       mutation_data = test_data_muts_ctcpg,
+                       muts_type = "All",
+                       sample_id = test_data_id,
+                       amplification_chrom = test_data_chrom,
+                       amplification_start = test_data_start_ctcpg,
+                       amplification_stop = test_data_stop_ctcpg,
+                       is_WGD = test_data_status,
+                       genome = "hg19")
+  ),"data.frame")
 })
