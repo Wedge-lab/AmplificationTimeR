efb6b9cd82d857069cab190fdc35e3301057e6c5
mjakobs <25904555+mjakobs@users.noreply.github.com>
2023-01-03 13:47:42 +0000

    added 3+0 test

--- tests/testthat/test_time_amplification.R
+++ tests/testthat/test_time_amplification.R
@@ -392,6 +392,30 @@ test_that("AmplificationTimeR runs and produces 2+2 highest output", {
                                   genome = "hg19")[,"event_order"],"Cannot be timed")
 })
 
+#### 3+0 ####
+test_that("AmplificationTimeR runs and produces 3+0 highest output", {
+  expect_equal(time_amplification(cn_data = test_data_cn_3_0,
+                                  multiplicity_data = test_data_mult,
+                                  mutation_data = test_data_muts,
+                                  muts_type = "SBS1 and SBS5",
+                                  sample_id = test_data_id,
+                                  amplification_chrom = test_data_chrom,
+                                  amplification_start = test_data_start,
+                                  amplification_stop = test_data_stop,
+                                  is_WGD = test_data_status,
+                                  genome = "hg19")[,"highest_copy_number"],"3+0")
+  expect_equal(time_amplification(cn_data = test_data_cn_3_0,
+                                  multiplicity_data = test_data_mult,
+                                  mutation_data = test_data_muts,
+                                  muts_type = "SBS1 and SBS5",
+                                  sample_id = test_data_id,
+                                  amplification_chrom = test_data_chrom,
+                                  amplification_start = test_data_start,
+                                  amplification_stop = test_data_stop,
+                                  is_WGD = test_data_status,
+                                  genome = "hg19")[,"event_order"],"GG")
+})
+
 #### 3+1 ####
 test_that("AmplificationTimeR runs and produces 3+1 highest output", {
   expect_equal(time_amplification(cn_data = test_data_cn_3_1,
