d76a25a6d0f0a2435aea012d17dc88b2d7354d42
mjakobs <25904555+mjakobs@users.noreply.github.com>
2023-01-03 13:52:14 +0000

    added 9+0 test

--- tests/testthat/test_time_amplification.R
+++ tests/testthat/test_time_amplification.R
@@ -703,6 +703,31 @@ test_that("AmplificationTimeR runs and produces 8+1 highest output", {
                                   is_WGD = test_data_status,
                                   genome = "hg19")[,"event_order"],"GGGGGGG - Unsure")
 })
+
+#### 9+0 ####
+test_that("AmplificationTimeR runs and produces 9+0 highest output", {
+  expect_equal(time_amplification(cn_data = test_data_cn_9_0,
+                                  multiplicity_data = test_data_mult,
+                                  mutation_data = test_data_muts,
+                                  muts_type = "SBS1 and SBS5",
+                                  sample_id = test_data_id,
+                                  amplification_chrom = test_data_chrom,
+                                  amplification_start = test_data_start,
+                                  amplification_stop = test_data_stop,
+                                  is_WGD = test_data_status,
+                                  genome = "hg19")[,"highest_copy_number"],"9+0")
+  expect_equal(time_amplification(cn_data = test_data_cn_9_0,
+                                  multiplicity_data = test_data_mult,
+                                  mutation_data = test_data_muts,
+                                  muts_type = "SBS1 and SBS5",
+                                  sample_id = test_data_id,
+                                  amplification_chrom = test_data_chrom,
+                                  amplification_start = test_data_start,
+                                  amplification_stop = test_data_stop,
+                                  is_WGD = test_data_status,
+                                  genome = "hg19")[,"event_order"],"GGGGGGGG - Unsure")
+})
+
 #### 9+1 ####
 test_that("AmplificationTimeR runs and produces 9+1 highest output", {
   expect_equal(time_amplification(cn_data = test_data_cn_9_1,
